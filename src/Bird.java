ae585fba70a3ad717e2fa06efcfbfb69d2eccfac
AndriyChmykhal <chmykhal@ukr.net>
2020-03-14 22:54:28 +0200

    birds in the zoo

--- src/Bird.java
+++ src/Bird.java
@@ -1,7 +1,9 @@
-public class Bird {
+import java.sql.SQLOutput;
+
+abstract class Bird {
   // TODO fix class declaration
 
   public void eat() {
-    // TODO print a message that bird is eating
+    System.out.println("I'm eating");
   }
 }
--- src/Zoo.java
+++ src/Zoo.java
@@ -2,40 +2,46 @@ import java.util.List;
 
 public class Zoo {
 
-  /**
-   * Feeds different bird sections of the zoo.
-   *
-   * @param list list of birds of particular type.
-   */
-  public static void feed(List list) {
-    // TODO fix method declaration
-    System.out.println("Feeding birds");
-    // TODO feed birds in the cycle
-  }
+  /*add a bird to the list
+feed different bird sections of the zoo
+add birds to a flying section of the zoo Uncomment tests and execute them. All test should pass.*/
 
-  /**
-   * Adds birds to a flying section of the zoo.
-   *
-   * @param list list of birds of particular type.
-   * @param bird a new bird.
-   */
-  public static void acceptBird(List list, FlyingBird bird) {
-    // TODO fix method declaration
-    System.out.println("Accepting a bird to a section");
-    // TODO check flying bird wings
-    // TODO add a bird to the list and print it
-  }
+    /**
+     * Feeds different bird sections of the zoo.
+     *
+     * @param list list of birds of particular type.
+     */
+    public static void feed(List<? extends Bird> list) {
+        for (Bird value : list) {
+            System.out.println("Feeding bird " + value);
+        }
+    }
 
-  /**
-   * Adds bird to the general list.
-   *
-   * @param list    list of birds.
-   * @param newBird bird to add.
-   */
-  public static void registerBird(List list, Bird newBird) {
-    // TODO fix method declaration
-    System.out.println("Adding a bird to the birds list");
-    // TODO add a bird to list
-    // TODO print birds in the cycle
-  }
+    /**
+     * Adds birds to a flying section of the zoo.
+     *
+     * @param list list of birds of particular type.
+     * @param bird a new bird.
+     */
+    public static void acceptBird(List<? super FlyingBird> list, FlyingBird bird) {
+
+        bird.checkWings(bird);
+        list.add(bird);
+        System.out.println("Accepting a " + bird.toString() + " to a section");
+    }
+
+    /**
+     * Adds bird to the general list.
+     *
+     * @param list    list of birds.
+     * @param newBird bird to add.
+     */
+    public static void registerBird(List<? super Bird> list, Bird newBird) {
+        System.out.println("Adding a to the birds list");
+        list.add(newBird);
+
+        for (Object generalBird : list) {
+            System.out.println(generalBird.toString());
+        }
+    }
 }
